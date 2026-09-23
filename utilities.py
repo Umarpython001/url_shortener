8ff7326df201562b04e9d08b81aa13a1ee45f7d9
Umar <olowonyoumar@gmail.com>
2026-09-23 01:21:56 +0100

    Utilites for some extra logic

---
+++ utilities.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlparse #Used to validate the long url that the user provides
+import random
+
+BASE_62 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+def generate_short_url():
+
+    ans = ""
+    for i in range(7):
+        ans += random.choice(BASE_62)
+
+    return ans, len(ans)
+
+def is_valid_url(url:str) -> bool:
+    try:
+        result = urlparse(url)
+        # Check if scheme and netloc (domain) are present
+        return all([result.scheme in ['http', 'https'], result.netloc])
+    except Exception:
+        return False
+
