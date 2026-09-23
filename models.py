2640f3ce949af9e3ee0212294f97e4888cc2bc94
Umar <olowonyoumar@gmail.com>
2026-09-23 01:21:25 +0100

    Url model file complete

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Integer, Date, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+
+
+class Maps(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    long_url = Column(String, index=True)
+    shortened_url = Column(String, unique=True, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
