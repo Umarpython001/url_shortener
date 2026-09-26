835a9ada28c5c91853038c35de9ea6cd1a1acdb6
Umar <olowonyoumar@gmail.com>
2026-09-26 04:04:55 +0100

    Changed DB url to switch to postgres

--- database.py
+++ database.py
@@ -1,11 +1,19 @@
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./shortener.db"
+load_dotenv()
+
+DB_PASSWORD = os.getenv("POSTGRES_DB_PASSWORD")
+
+# postgresql://[username]:[password]@[hostname]:[port]/[database_name]
+
+DATABASE_URL = f"postgresql://postgres:{DB_PASSWORD}@localhost:5432/url_shortener"
 
 engine = create_engine(
     DATABASE_URL, 
-    connect_args={"check_same_thread": False}
+    # connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
